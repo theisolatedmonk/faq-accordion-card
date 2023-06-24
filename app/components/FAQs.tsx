2366fdab0df3eb43b37d931419efe4eb9be309cc
theisolatedmonk <96rahulseth@gmail.com>
2023-06-24 22:52:29 +0530

    question-dropdown-implimented

--- app/components/FAQs.tsx
+++ app/components/FAQs.tsx
@@ -1,7 +1,10 @@
-
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import errow from '@/app/components/images/icon-arrow-down.svg'
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Image from 'next/image'
+import useOnclickOutside from 'react-cool-onclickoutside'
+import clsx from 'clsx';
 
 type FAQTypes = {
     Questions : string
@@ -11,16 +14,41 @@ type FAQTypes = {
 
  export default function 
 (props:FAQTypes) {
+
+    const [isDropDownOpen, setDropDown] = useState(false);
+    // const [isBold, setBold] = useState(false);
+
+    const [animationParent] = useAutoAnimate();
+    const ref = useOnclickOutside(() => {
+      setDropDown(false);
+    }); 
+  
+    function openDropDown() {
+      console.log("drop down clicked", isDropDownOpen.toString());
+  
+      setDropDown(!isDropDownOpen);
+       
+    }
+
   return (
-   
-        <div className={`flex flex-col w-full`}>
+   <section  ref={animationParent}
+   onClick={openDropDown}
+   className="relative cursor-pointer ">
+ <div className={`flex flex-col w-full`}>
            <div className="flex items-center gap-2"> 
-           <p className="">{props.Questions}</p>
-           <Image src={errow} alt={''} className='cursor-auto'/>
+           <p className={` ${props.class} `}>{props.Questions}</p>
+           <Image src={errow} alt={''} className={clsx(" transition-all", {
+              "rotate-180 ": isDropDownOpen,
+            })}/>
            </div>
-            <p className={`flex flex-col ${props.class} `}>{props.Answer}</p>
+           {isDropDownOpen && (
+            <p className={`flex flex-col ${props.class} `}>{props.Answer}</p>)}
 
         </div>
+
+   </section>
+       
     
   )
 }
+
